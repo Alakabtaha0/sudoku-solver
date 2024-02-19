75d4d31814590654cd62509501fcb727be941049
Taha Al Akab <alakabtaha@gmail.com>
2024-02-19 00:36:19 +0000

    Game Play Done

--- main.py
+++ main.py
@@ -6,29 +6,25 @@ import sys
 # make background black
 # Set pixels
 
-
-# Main Function
-def main():
-    grid = [
-        [0, 0, 6, 0, 0, 0, 5, 0, 8],
-        [1, 0, 2, 3, 8, 0, 0, 0, 4],
-        [0, 0, 0, 2, 0, 0, 1, 9, 0],
-        [0, 0, 0, 0, 6, 3, 0, 4, 5],
-        [0, 6, 3, 4, 0, 5, 8, 7, 0],
-        [5, 4, 0, 9, 2, 0, 0, 0, 0],
-        [0, 8, 7, 0, 0, 4, 0, 0, 0],
-        [2, 0, 0, 0, 9, 8, 4, 0, 7],
-        [4, 0, 9, 0, 0, 0, 3, 0, 0],
+grid = [
+        [{"num": None, "init": False}, {"num": None, "init": False}, {"num": 6, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": 5, "init": True}, {"num": None, "init": False}, {"num": 8, "init": True}],
+        [{"num": 1, "init": True}, {"num": None, "init": False}, {"num": 2, "init": True}, {"num": 3, "init": True}, {"num": 8, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": 4, "init": True}],
+        [{"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": 2, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": 1, "init": True}, {"num": 9, "init": True}, {"num": None, "init": False}],
+        [{"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": 6, "init": True}, {"num": 3, "init": True}, {"num": None, "init": False}, {"num": 4, "init": True}, {"num": 5, "init": True}],
+        [{"num": None, "init": False}, {"num": 6, "init": True}, {"num": 3, "init": True}, {"num": 4, "init": True}, {"num": None, "init": False}, {"num": 5, "init": True}, {"num": 8, "init": True}, {"num": 7, "init": True}, {"num": None, "init": False}],
+        [{"num": 5, "init": True}, {"num": 4, "init": True}, {"num": None, "init": False}, {"num": 9, "init": True}, {"num": 2, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}],
+        [{"num": None, "init": False}, {"num": 8, "init": True}, {"num": 7, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": 4, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}],
+        [{"num": 2, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": 9, "init": True}, {"num": 8, "init": True}, {"num": 4, "init": True}, {"num": None, "init": False}, {"num": 7, "init": True}],
+        [{"num": 4, "init": True}, {"num": None, "init": False}, {"num": 9, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": None, "init": False}, {"num": 3, "init": True}, {"num": None, "init": False}, {"num": None, "init": False}]
     ]
 
-    # Initialize Pygame
-    pg.init()
+
+
+def game_loop():
     screen = pg.display.set_mode((800, 600))
-    pg.display.set_caption("Sudoku Solver")
     clock = pg.time.Clock()
     running = True
     SELECTED = None
-
     # Game Loop
     while running:
         # Check for event
@@ -42,13 +38,19 @@ def main():
             if event.type == pg.MOUSEBUTTONDOWN:
                 # Get the position of the mouse
                 x, y = pg.mouse.get_pos()
-                print(f"x: {x}, y: {y}")
+                if x > 450 or y > 450: continue
                 # Floor the position to get the row and column
                 row = y // 50
                 col = x // 50
-                SELECTED = [row, col]
-                print(f"SELECTED:: {SELECTED}")
-                print(grid[row][col])
+                if grid[row][col]['init'] != True: SELECTED = [row, col]
+            
+            # Check for number input
+            if event.type == pg.KEYDOWN and SELECTED is not None:
+                # If the key is a number and the cell is not an initial number
+                if event.unicode.isdigit() and event.unicode != '0' and grid[SELECTED[0]][SELECTED[1]]['init'] == False:
+                    # Change the value of the cell to the number
+                    grid[SELECTED[0]][SELECTED[1]]['num'] = int(event.unicode)
+                    SELECTED = None
 
         # RENDERING
         # Update the screen with the new frame
@@ -58,36 +60,52 @@ def main():
         for row in range(len(grid)):
             # Goes through each column
             for col in range(len(grid[row])):
+                # If the cell is selected
                 if SELECTED is not None and [row, col] == SELECTED:
                     # Draw a grey rectangle in the selected cell
-                    pg.draw.rect(screen, "grey", (col * 50, row * 50, 50, 50))
+                    pg.draw.rect(screen, "black", (col * 50 +1, row * 50 +1, 50, 50))
+                    pg.draw.rect(screen, "grey", (col * 50, row * 50, 49, 49))
                     # Create a font object to draw the number
                     font = pg.font.Font(None, 36)
-                    # Render the number
-                    text = font.render(str(grid[row][col]), True, "black")
-                    # Draw the number ontop of the white rectangle along with the offset
-                    screen.blit(text, (col * 50 + 17, row * 50 + 13))
+                    # if it's not None, draw the number else draw nothing
+                    screen.blit(font.render(str(grid[row][col]['num']), True, "black"), (col * 50 + 17, row * 50 + 13)) if grid[row][col]['num'] is not None else None
                     continue
                 # If the value is 0, draw a black rectangle
-                if grid[row][col] != 0:
+                if grid[row][col]['num'] != None:
                     # Draw a white rectangle which is the background
-                    pg.draw.rect(screen, "white", (col * 50, row * 50, 50, 50))
+                    pg.draw.rect(screen, "black", (col * 50 +1, row * 50 +1, 50, 50))
+                    pg.draw.rect(screen, "white", (col * 50, row * 50, 49, 49))
                     # Create a font object to draw the number
                     font = pg.font.Font(None, 36)
                     # Render the number
-                    text = font.render(str(grid[row][col]), True, "black")
+                    text = font.render(str(grid[row][col]['num']), True, "black")
                     # Draw the number ontop of the white rectangle along with the offset
                     screen.blit(text, (col * 50 + 17, row * 50 + 13))
                 else:
-                    # Draw a black rectangle
-                    pg.draw.rect(screen, "black", (col * 50, row * 50, 50, 50))
-                    font = pg.font.Font(None, 36)
+                    # Draw a white rectangle
+                    pg.draw.rect(screen, "black", (col * 50 +1, row * 50 +1, 50, 50))
+                    pg.draw.rect(screen, "white", (col * 50, row * 50, 49, 49))
+                    
 
         # Draw the new frame
         pg.display.flip()
 
         # Frame rate
         clock.tick(30)
+    return
+
+# Main Function
+def main():
+    # Sudoku Grid
+    # Set object so that we can keep track of the initial numbers and no changes can be made
+    
+
+    # Initialize Pygame
+    pg.init()
+    pg.display.set_caption("Sudoku Solver")
+    
+    game_loop()
+    
 
     pg.quit()
 
